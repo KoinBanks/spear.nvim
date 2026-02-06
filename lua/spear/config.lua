92b12d275bb0101437569052a488a7805d4706bf
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-02-06 23:04:58 +0100

    No need for comments here

--- lua/spear/config.lua
+++ lua/spear/config.lua
@@ -2,18 +2,12 @@ local M = {}
 
 ---@type SpearConfig
 M.defaults = {
-	-- Path to save spear list file
 	save_path = vim.fn.stdpath("data") .. "/spear.list",
-	-- Auto-remove files that no longer exist
 	prune_missing = false,
 	ui = {
-		-- Border style: "single", "double", "rounded", "solid", "shadow"
 		border = "rounded",
-		-- Window width ratio
 		width_ratio = 0.5,
-		-- Window height ratio
 		height_ratio = 0.2,
-		-- Window title
 		title = " Spear ",
 	},
 }
